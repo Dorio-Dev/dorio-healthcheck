becf9f5d326bd1a14c8e2c16686e17d5ad09b2cb
dohk0826 <dohk0826@gmail.com>
2019-10-15 21:29:24 +0900

    added config.yaml    - defined process to check modified app.py    - check process and return process count

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import subprocess
+import yaml
 from flask import Flask
 
 app = Flask(__name__)
@@ -5,7 +7,25 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    yml = open('config.yaml', 'r')
+    #result = subprocess.check_output("ps -ef | grep bash", shell=True)
+    ymlResult = yaml.load(yml)
+    processes = ymlResult['processes']
+
+    result = "test <br/>"
+    for p in processes:
+        result += "name : " +  p['name']
+        str = subprocess.check_output("ps -ef | grep " + p['ps'] + "| grep -v grep | wc -l", shell=True)
+        result += "<br/>ps count : " + str.decode('utf-8')
+        result += "<br/>"
+
+
+
+
+    #print(ymlResult['test'])
+    #result = subprocess.check_output("ps -ef | grep " + processes['cmd'] + "| grep -v grep | wc -l", shell=True)
+
+    return result
 
 
 if __name__ == '__main__':
