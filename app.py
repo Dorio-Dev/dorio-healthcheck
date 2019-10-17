5398e19a9d46f1cd62e6b8a3927ad03682853a90
Dorio-Dev <56225119+Dorio-Dev@users.noreply.github.com>
2019-10-17 11:05:09 +0900

    Update app.py
    Seted host = any, port =8000, return status code

--- app.py
+++ app.py
@@ -1,32 +1,28 @@
 import subprocess
 import yaml
+from flask_api import status
 from flask import Flask
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
+@app.route('/HealthCheck')
+def healthCheck():
     yml = open('config.yaml', 'r')
-    #result = subprocess.check_output("ps -ef | grep bash", shell=True)
     ymlResult = yaml.load(yml)
     processes = ymlResult['processes']
 
-    result = "test <br/>"
+    result = "Process Monitoring <br/>"
     for p in processes:
         result += "name : " +  p['name']
         str = subprocess.check_output("ps -ef | grep " + p['ps'] + "| grep -v grep | wc -l", shell=True)
         result += "<br/>ps count : " + str.decode('utf-8')
         result += "<br/>"
-
-
-
-
-    #print(ymlResult['test'])
-    #result = subprocess.check_output("ps -ef | grep " + processes['cmd'] + "| grep -v grep | wc -l", shell=True)
+        if int(str) < 1 :
+                return result, status.HTTP_503_SERVICE_UNAVAILABLE
 
     return result
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0',port='8000')
